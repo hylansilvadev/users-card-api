0aa167a60781cc89d520890141ece9a08d5e3758
Hylan Silva <hylansilva@outlook.com>
2024-02-28 19:51:08 -0300

    🚧 [Feature] - Inicio das rotas de usuário

--- api/app.py
+++ api/app.py
@@ -1,35 +1,23 @@
-from typing import List, Optional
+from typing import List
 
 import firebase_admin
 from fastapi import FastAPI
-from firebase_admin import credentials, firestore
-from firedantic import Model
-from pydantic import BaseModel
+from firebase_admin import credentials, firestore, storage
+from firedantic import configure
 
+from api.user import User, UserCreate
 from core.Settings import settings
 
 cred = credentials.Certificate(
     'core/db-card-dev-firebase-adminsdk-c91lk-7184239cbb.json'
 )
 
+
 firebase_admin.initialize_app(cred)
-
 app = FastAPI(title=settings.PROJECT_NAME)
-
 db = firestore.client()
-
-
-class User(BaseModel):
-    name: str
-    tags: List
-    linkedin: Optional[str] = None
-    instagram: Optional[str] = None
-    github: Optional[str] = None
-
-
-class Users(Model):
-    __collection__ = 'users'
-    users: List[User]
+bucket = storage.bucket('db-card-dev')
+configure(db)
 
 
 @app.get('/')
@@ -37,8 +25,17 @@ def root() -> str:
     return 'Hello World'
 
 
-@app.get('/users')
-def get_users():
-    users = db.collection('users').get()
-    data = [user.to_dict() for user in users]
-    return data
+@app.get('/users', tags=["User Routes"])
+def get_all_users() -> List[User]:
+    return User.find()
+
+
+@app.get('/users/{user_id}', tags=["User Routes"])
+def get_user_by_id(user_id: str) -> str:
+    user = User.get_by_doc_id(user_id)
+    nick = user.name.lower().replace(' ', '')
+    image = bucket.blob(nick).public_url
+    return image
+
+
+# to-do: fazer o restante das rotas de Users
